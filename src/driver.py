5bd0b49ef97612c970247cf4661a1ffc7c3393fa
Abdullah Ahmed <abdullah_ahmed@brown.edu>
2019-11-29 02:12:59 -0500

    BIO labels

--- src/driver.py
+++ src/driver.py
@@ -1,7 +1,10 @@
-import preprocess
+from preprocess import PreProcessor
 
 def main():
-    preprocess.get_data("../training-PHI-Gold-Set1/")
+    pp = PreProcessor()
+    s_array, t_array, labels = pp.process_data("../training-PHI-Gold-Set1/")
+    pp.create_vocab_dict()
+    pp.create_train_set(t_array,labels)
 
 if __name__ == "__main__":
     main()
