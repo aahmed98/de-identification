cf77bac4da89a221f8e6e359d0745d01f86a0af1
aahmed98 <aa146@gpu1207.oscar.ccv.brown.edu>
2020-04-01 14:31:28 -0400

    less complex

--- src/models/bilstm_chars.py
+++ src/models/bilstm_chars.py
@@ -19,7 +19,7 @@ class BiLSTM_Chars(tf.keras.Model):
                 default_value=tf.constant("UNK"),
                 name="idx2word"
             ) # must do this for tensorflow operation
-        self.embedding_size = 50
+        self.embedding_size = 30
         self.rnn_size = 64
         self.title = "bi-lstm-chars"
 
